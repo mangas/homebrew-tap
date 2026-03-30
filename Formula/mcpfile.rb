410d4d0041f8791a235dc980f9381809b88ab17b
Filipe Azevedo <filipe@azevedo.io>
2026-03-30 20:34:52 +0100

    Update mcpfile to v0.2.0

--- Formula/mcpfile.rb
+++ Formula/mcpfile.rb
@@ -7,14 +7,14 @@ class Mcpfile < Formula
   on_macos do
     on_arm do
       url "https://github.com/mangas/mcpfile/releases/download/mcpfile-v#{version}/mcpfile-aarch64-darwin"
-      sha256 "PLACEHOLDER"
+      sha256 "95bdd3b87488902be00717ec3f61837698d863f1ebabb0d88e88bf644587af5c"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/mangas/mcpfile/releases/download/mcpfile-v#{version}/mcpfile-x86_64-linux"
-      sha256 "PLACEHOLDER"
+      sha256 "ba019cbc944de469ab59c6331e82133a355a453aec69cefbb81b5c88b7750e02"
     end
   end
 
